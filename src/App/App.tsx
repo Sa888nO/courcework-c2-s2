a33a064c0128834603d3efe0a1d39c0c3545d082
Sa888nO <shura.reno.97@bk.ru>
2022-06-20 09:29:31 +0300

    add button component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./App.css";
 import Button from "@components/Button";
 import "@styles/style.scss";
+import "@styles/_settings.scss";
 
 function App() {
-  return <Button />;
+  return <Button styles={"Button_cross"} />;
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,18 @@
-function Button() {
-  return <div className="App"> BUTTON</div>;
-}
+import React from "react";
+import "./Button.scss";
+
+export type ButtonProps = {
+  styles: string;
+  title?: string;
+  onClick?: (e: React.MouseEvent) => void;
+};
+
+const Button: React.FC<ButtonProps> = ({ title, styles, onClick }) => {
+  return (
+    <button className={`Button ${styles}`} onClick={onClick}>
+      {title}
+    </button>
+  );
+};
 
 export default Button;
