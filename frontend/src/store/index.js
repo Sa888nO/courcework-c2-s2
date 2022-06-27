e9ed536b1d5caacff783df31d03500573e3aecd8
Sa888nO <shura.reno.97@bk.ru>
2022-06-27 21:23:39 +0300

    delete backend files

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,13 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { itemsReducer } from "./items/itemsReducer";
-import { combineReducers } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   items: itemsReducer,
 });
-export const store = createStore(rootReducer);
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
--- frontend/src/store/items/itemsReducer.js
+++ frontend/src/store/items/itemsReducer.js
@@ -1,6 +1,6 @@
 // покупка (снимаем деньги со счета, удаляем товары)
 import { defaultState } from "./items";
 
-export const itemsReducer = (state = defaultState) => {
+export const itemsReducer = (state = defaultState, action) => {
   return state;
 };
