6c089925ca5962bce5a12dac62f98fea01498275
Sa888nO <shura.reno.97@bk.ru>
2022-06-28 16:30:12 +0300

    закончил api users (get/post)и posts (CRUD)

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 users = [
     {
-        'id': '1',
+        'id': 1,
         'name': 'Sasha',
         'password': '12345'
     },
     {
-        'id': '2',
+        'id': 2,
         'name': 'Petya',
         'password': '123456789'
     }
@@ -17,25 +17,25 @@ users = [
 
 posts = [
     {
-        'id': "1",
+        'id': 1,
         'user_id': "1",
         "title": "post1",
         "text": "text1"
     },
         {
-        'id': "2",
+        'id': 2,
         'user_id': "1",
         "title": "post2",
         "text": "text2"
     },
         {
-        'id': "3",
+        'id': 3,
         'user_id': "1",
         "title": "post3",
         "text": "text3"
     },
         {
-        'id': "4",
+        'id': 4,
         'user_id': "2",
         "title": "post4",
         "text": "text4"
@@ -65,11 +65,21 @@ def update_postrs_list():
     posts.append(new_one)
     return jsonify(posts)
 
-@app.route('/posts', methods=['PUT'])
-def update_postrs_list():
-    new_one = request.json
-    posts.append(new_one)
-    return jsonify(posts)
+@app.route('/posts/<int:post_id>', methods=['PUT'])
+def update_post(post_id):
+    item = next((x for x in posts if x['id'] == post_id), None)
+    params = request.json
+    if not item:
+        return {'massage': 'No posts with this id'}, 400
+    item.update(params)
+    return jsonify(item)
+
+@app.route('/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    idx, _ = next((x for x in enumerate(posts) if x[1]['id'] == post_id), (None, None))
+    posts.pop(idx)
+    return '', 204
+
 
 if __name__ == '__main__':
     app.run()
