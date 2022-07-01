e8968f7f7459d578d90528be967bc9390cea5ec5
Sa888nO <shura.reno.97@bk.ru>
2022-07-01 15:35:24 +0300

    Изменил бд под новую тему работы

--- backend/app.py
+++ backend/app.py
@@ -18,87 +18,91 @@ from models import *
 
 Base.metadata.create_all(bind=engine)
 
+# api /user 
+# api /lessons
+# api /subscribes
 
+# @app.route()
 
-# api /users
-@app.route('/users', methods=['GET'])
-def get_users_list():
-    users = User.query.all()
-    serialized = []
-    for user in users: 
-        serialized.append({
-            'id': user.id,
-            'name': user.name,
-            'password': user.password
-        })
-    return jsonify(serialized)
+# # api /users
+# @app.route('/users', methods=['GET'])
+# def get_users_list():
+#     users = User.query.all()
+#     serialized = []
+#     for user in users: 
+#         serialized.append({
+#             'id': user.id,
+#             'name': user.name,
+#             'password': user.password
+#         })
+#     return jsonify(serialized)
 
-@app.route('/users', methods=['POST'])
-def update_users_list():
-    new_one = User(**request.json)
-    session.add(new_one)
-    session.commit()
-    serialized = {
-        'id': new_one.id,
-        'name': new_one.name,
-        'password': new_one.password  
-    }
-    return jsonify(serialized)
+# @app.route('/users', methods=['POST'])
+# def update_users_list():
+#     new_one = User(**request.json)
+#     session.add(new_one)
+#     session.commit()
+#     serialized = {
+#         'id': new_one.id,
+#         'name': new_one.name,
+#         'password': new_one.password  
+#     }
+#     return jsonify(serialized)
 
-# api /posts
-@app.route('/posts', methods=['GET'])
-def get_posts_list():
-    posts = Post.query.all()
-    serialized = []
-    for post in posts: 
-        serialized.append({
-            'id': post.id,
-            'user_id': post.user_id,
-            'title': post.title,
-            'text': post.text
-        })
-    return jsonify(serialized)
+# # api /posts
+# @app.route('/posts', methods=['GET'])
+# def get_posts_list():
+#     posts = Post.query.all()
+#     serialized = []
+#     for post in posts: 
+#         serialized.append({
+#             'id': post.id,
+#             'user_id': post.user_id,
+#             'title': post.title,
+#             'text': post.text
+#         })
+#     return jsonify(serialized)
 
-@app.route('/posts', methods=['POST'])
-def update_postrs_list():
-    new_one = Post(**request.json)
-    session.add(new_one)
-    session.commit()
-    serialized = {
-        'id': new_one.id,
-        'user_id': new_one.user_id,
-        'title': new_one.title,
-        'text': new_one.text
-    }
-    return jsonify(serialized)
+# @app.route('/posts', methods=['POST'])
+# def update_postrs_list():
+#     new_one = Post(**request.json)
+#     session.add(new_one)
+#     session.commit()
+#     serialized = {
+#         'id': new_one.id,
+#         'user_id': new_one.user_id,
+#         'title': new_one.title,
+#         'text': new_one.text
+#     }
+#     return jsonify(serialized)
 
-@app.route('/posts/<int:post_id>', methods=['PUT'])
-def update_post(post_id):
-    item = Post.query.filter(Post.id == post_id).first()
-    params = request.json
-    if not item:
-        return {'massage': 'No posts with this id'}, 400
-    for key, value in params.items():
-        setattr(item, key, value)
-    session.commit()
-    serialized = {
-    'id': item.id,
-    'user_id': item.user_id,
-    'title': item.title,
-    'text': item.text
-    }
-    return jsonify(serialized)
+# @app.route('/posts/<int:post_id>', methods=['PUT'])
+# def update_post(post_id):
+#     item = Post.query.filter(Post.id == post_id).first()
+#     params = request.json
+#     if not item:
+#         return {'massage': 'No posts with this id'}, 400
+#     for key, value in params.items():
+#         setattr(item, key, value)
+#     session.commit()
+#     serialized = {
+#     'id': item.id,
+#     'user_id': item.user_id,
+#     'title': item.title,
+#     'text': item.text
+#     }
+#     return jsonify(serialized)
 
-@app.route('/posts/<int:post_id>', methods=['DELETE'])
-def delete_post(post_id):
-    item = Post.query.filter(Post.id == post_id).first()
-    if not item:
-        return {'massage': 'No posts with this id'}, 400
-    session.delete(item)
-    session.commit()
-    return '', 204
+# @app.route('/posts/<int:post_id>', methods=['DELETE'])
+# def delete_post(post_id):
+#     item = Post.query.filter(Post.id == post_id).first()
+#     if not item:
+#         return {'massage': 'No posts with this id'}, 400
+#     session.delete(item)
+#     session.commit()
+#     return '', 204
 
-
+# session close
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     session.remove()
--- backend/models.py
+++ backend/models.py
@@ -4,11 +4,19 @@ class User(Base):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-class Post(Base):
-    __tablename__ = "posts"
+class Lesson(Base):
+    __tablename__ = "lessons"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     title = db.Column(db.String(50), nullable=False)
-    text = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    couch = db.Column(db.String(500), nullable=False)
+
+class Subscribe(Base):
+    __tablename__ = "subscribes"
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), primary_key=True, nullable=False)
+    lesson_id = db.Column(db.Integer, db.ForeignKey(Lesson.id), primary_key=True, nullable=False)
+
+    
