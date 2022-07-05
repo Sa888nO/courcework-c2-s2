022bed13792fc23ddfdb38fe6d63c7742930171d
Sa888nO <shura.reno.97@bk.ru>
2022-07-05 12:05:09 +0300

    удалил лишнее

--- frontend/src/components/LessonCard/LessonCard.js
+++ frontend/src/components/LessonCard/LessonCard.js
@@ -1,5 +1,19 @@
-const LessonCard = () => {
-  return <div>LessonCard</div>;
+import "./LessonCard.scss";
+const LessonCard = ({ title, coach, date, subscribe }) => {
+  return (
+    <div className="lesson-card">
+      <div className="info-block">
+        <span className="info-block__title">{title}</span>
+        <span className="info-block__coach">{coach}</span>
+        <span className="info-block__date">{date}</span>
+      </div>
+      {subscribe === true ? (
+        <button className="button button_out">Отписаться</button>
+      ) : (
+        <button className="button button_in">Записаться</button>
+      )}
+    </div>
+  );
 };
 
 export default LessonCard;
--- frontend/src/pages/Authorization/Authorization.js
+++ frontend/src/pages/Authorization/Authorization.js
@@ -2,9 +2,12 @@ import "./Authorization.scss";
 import LessonCard from "@components/LessonCard";
 const Authorization = () => {
   return (
-    <div className="wrapper">
-      <LessonCard />
-    </div>
+    <LessonCard
+      title={"Качалка"}
+      coach={"Павел"}
+      date={"06-07-2022 12:00:00"}
+      subscribe={true}
+    />
   );
 };
 
