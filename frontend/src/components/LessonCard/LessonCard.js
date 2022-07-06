acd4d686fd5aaf6d8f9c7d934e655888cae4dfaf
Sa888nO <shura.reno.97@bk.ru>
2022-07-06 23:34:41 +0300

    заканчиваю часть фронта

--- frontend/src/components/LessonCard/LessonCard.js
+++ frontend/src/components/LessonCard/LessonCard.js
@@ -1,5 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RefreshData } from "@pages/RefreshData";
 import "./LessonCard.scss";
-const LessonCard = ({ title, coach, date, subscribe }) => {
+const LessonCard = ({ title, coach, date, subscribe, ID }) => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+  let id = ID;
+  const currDate = new Date().toLocaleDateString();
+  const currTime = new Date().toLocaleTimeString();
+  console.log(currDate + currTime);
+
+  const subOnLesson = (id) => {
+    let body = {
+      user_id: user.id,
+      lesson_id: id,
+    };
+    fetch("http://127.0.0.1:5000/api/subscribe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then(function (response) {
+      RefreshData(dispatch);
+    });
+  };
+  const DeleteSubOnLesson = (id) => {
+    let body = {
+      user_id: user.id,
+      lesson_id: id,
+    };
+    fetch("http://127.0.0.1:5000/api/DeleteSubscribe", {
+      method: "Delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then(function (response) {
+      RefreshData(dispatch);
+    });
+  };
   return (
     <div className="lesson-card">
       <div className="info-block">
@@ -8,9 +47,16 @@ const LessonCard = ({ title, coach, date, subscribe }) => {
         <span className="info-block__date">{date}</span>
       </div>
       {subscribe === true ? (
-        <button className="button button_out">Отписаться</button>
+        <button
+          className="button button_out"
+          onClick={() => DeleteSubOnLesson(id)}
+        >
+          Отписаться
+        </button>
       ) : (
-        <button className="button button_in">Записаться</button>
+        <button className="button button_in" onClick={() => subOnLesson(id)}>
+          Записаться
+        </button>
       )}
     </div>
   );
--- frontend/src/pages/MainPage/MainPage.js
+++ frontend/src/pages/MainPage/MainPage.js
@@ -1,17 +1,80 @@
 import Header from "@components/Header";
+import "./MainPage.scss";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getLessonsAction } from "./../../store/lessonReducer";
+import LessonCard from "@components/LessonCard";
+import { RefreshData } from "@pages/RefreshData";
 
 const MainPage = () => {
   const dispatch = useDispatch();
   const lessons = useSelector((state) => state.lessons.lessons);
   const subscribes = useSelector((state) => state.subscribes.subscribes);
+  const user = useSelector((state) => state.user.user);
+  console.log(user);
+
+  const Lessons = (lessons, subscribes, user, type) => {
+    let user_subs = [];
+    let answer = [];
+    let answerFree = [];
+    for (let i = 0; i < subscribes.length; i++) {
+      if (user.id == subscribes[i].user_id) {
+        user_subs.push(subscribes[i].lesson_id);
+      }
+    }
+    for (let i = 0; i < lessons.length; i++) {
+      if (user_subs.includes(lessons[i].id)) {
+        answer.push(lessons[i]);
+      } else answerFree.push(lessons[i]);
+    }
+    if (type === "free") {
+      return answerFree;
+    } else if (type === "sub") {
+      return answer;
+    }
+  };
 
   return (
     <div>
       <Header title={"Страница записи"} />
-      <button>334</button>
+      <div className="content">
+        <div className="block">
+          <div className="block__title">Записаться</div>
+          <div className="block__lessons">
+            {Lessons(lessons, subscribes, user, "free").length > 0 ? (
+              Lessons(lessons, subscribes, user, "free").map((lesson) => (
+                <LessonCard
+                  title={lesson.title}
+                  coach={lesson.coach}
+                  date={lesson.date}
+                  subscribe={false}
+                  ID={lesson.id}
+                />
+              ))
+            ) : (
+              <div>Доступных для записи занятий нет</div>
+            )}
+          </div>
+        </div>
+        <div className="block">
+          <div className="block__title">Уже записан</div>
+          <div className="block__lessons">
+            {Lessons(lessons, subscribes, user, "sub").length > 0 ? (
+              Lessons(lessons, subscribes, user, "sub").map((lesson) => (
+                <LessonCard
+                  title={lesson.title}
+                  coach={lesson.coach}
+                  date={lesson.date}
+                  subscribe={true}
+                  ID={lesson.id}
+                />
+              ))
+            ) : (
+              <div>Вы никуда не записаны</div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
